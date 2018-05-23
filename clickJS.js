dc6fbe77979ac77aa3586b53462d5f472cbe4044
tduval <duval.thomas01@gmail.com>
2018-05-23 18:04:10 +0800

    Delete html includes and enable js logic

---
+++ clickJS.js
@@ -0,0 +1,33 @@
+var totalClick = 0;
+var resMoney = 0;
+var resEnergy = 0;
+var resXXX = 0;
+var resYYY = 0;
+
+function getTotalClick(){
+    return totalClick;
+}
+
+function incrementResMoney(){
+    totalClick++;
+    resMoney++;
+    return resMoney;
+}
+
+function incrementResEnergy(){
+    totalClick++;
+    resEnergy++;
+    return resEnergy;
+}
+
+function incrementResXXX(){
+    totalClick++;
+    resXXX++;
+    return resXXX;
+}
+
+function incrementResYYY(){
+    totalClick++;
+    resYYY++;
+    return resYYY;
+}
