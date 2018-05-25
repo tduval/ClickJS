a2695586fcce127233d66125a6388c86351d3cbd
Thomas Duval <Thomas.Duval@emeriocorp.com>
2018-05-25 09:32:48 +0800

    Rename increment to buy function

--- clickJS.js
+++ clickJS.js
@@ -1,33 +1,51 @@
 var totalClick = 0;
-var resMoney = 0;
 var resEnergy = 0;
+var resCellB = 0;
+var resCellT = 0;
 var resXXX = 0;
 var resYYY = 0;
+
+var resCellBEnergyCost = 10;
+var resCellTEnergyCost = 50;
+var resXXXEnergyCost = 250;
+var resYYYEnergyCost = 1000;
+
+var resEnergySteps = [
+    { id : 0, name : "CellB", energyRequired : 10},
+    { id : 1, name : "CellT", energyRequired : 50},
+    { id : 2, name : "XXX", energyRequired : 250},
+    { id : 3, name : "YYY", energyRequired : 1000},
+];
 
 function getTotalClick(){
     return totalClick;
 }
-
-function incrementResMoney(){
-    totalClick++;
-    resMoney++;
-    return resMoney;
-}
-
 function incrementResEnergy(){
     totalClick++;
     resEnergy++;
     return resEnergy;
 }
-
-function incrementResXXX(){
+function buyResCellB(){
+    totalClick++;
+    resCellB++;
+    resEnergy-=resCellBEnergyCost;
+    return resCellB;
+}
+function buyResCellT(){
+    totalClick++;
+    resCellT++;
+    resEnergy-=resCellTEnergyCost;
+    return resCellT;
+}
+function buyResXXX(){
     totalClick++;
     resXXX++;
+    resEnergy-=resXXXEnergyCost;
     return resXXX;
 }
-
-function incrementResYYY(){
+function buyResYYY(){
     totalClick++;
     resYYY++;
+    resEnergy-=resYYYEnergyCost;
     return resYYY;
 }
