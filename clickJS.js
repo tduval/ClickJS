0a12f260ad6a9e1c63dce2a60b62e584e20d76c0
Thomas Duval <Thomas.Duval@emeriocorp.com>
2018-05-25 16:07:18 +0800

    Rewrite clickJS for VUEX

--- clickJS.js
+++ clickJS.js
@@ -1,51 +1,75 @@
-var totalClick = 0;
-var resEnergy = 0;
-var resCellB = 0;
-var resCellT = 0;
-var resXXX = 0;
-var resYYY = 0;
+const labor =   {
+        id: 0,
+        name: 'Manual Labor',
+        costEnergy: 10,
+        earningMoney: 10,
+        purchased: 0
+    }
 
-var resCellBEnergyCost = 10;
-var resCellTEnergyCost = 50;
-var resXXXEnergyCost = 250;
-var resYYYEnergyCost = 1000;
+const producers = [
+    {
+        id: 1,
+        name: 'Bicycle',
+        costMoney: 50,
+        costEnergy: 8,
+        moneyPerSeconds: 0,
+        energyPerSeconds: 1,
+        earningMoney: 15,
+        earningEnergy:0,
+        purchased: 0
+    },
+    {
+        id: 3,
+        name: 'Scooter',
+        costMoney: 500,
+        costEnergy: 5,
+        moneyPerSeconds: 0,
+        energyPerSeconds: 2,
+        earningMoney: 25,
+        earningEnergy:0,
+        purchased: 0
+    },
+    {
+        id: 4,
+        name: 'Car',
+        costMoney: 1000,
+        costEnergy: 10,
+        moneyPerSeconds: 0,
+        energyPerSeconds: 3,
+        earningMoney: 100,
+        earningEnergy:0,
+        purchased: 0
+    },
+]
 
-var resEnergySteps = [
-    { id : 0, name : "CellB", energyRequired : 10},
-    { id : 1, name : "CellT", energyRequired : 50},
-    { id : 2, name : "XXX", energyRequired : 250},
-    { id : 3, name : "YYY", energyRequired : 1000},
-];
-
-function getTotalClick(){
-    return totalClick;
-}
-function incrementResEnergy(){
-    totalClick++;
-    resEnergy++;
-    return resEnergy;
-}
-function buyResCellB(){
-    totalClick++;
-    resCellB++;
-    resEnergy-=resCellBEnergyCost;
-    return resCellB;
-}
-function buyResCellT(){
-    totalClick++;
-    resCellT++;
-    resEnergy-=resCellTEnergyCost;
-    return resCellT;
-}
-function buyResXXX(){
-    totalClick++;
-    resXXX++;
-    resEnergy-=resXXXEnergyCost;
-    return resXXX;
-}
-function buyResYYY(){
-    totalClick++;
-    resYYY++;
-    resEnergy-=resYYYEnergyCost;
-    return resYYY;
-}
+const store = new Vuex.Store({
+  state: {
+    clickAmount: 0,
+    moneyAmount: 0,
+    energyAmount: 10,
+    producers: producers
+  },
+  mutations: {
+    incrementEnergy (state, amount=1){
+        state.energyAmount+=amount
+        state.clickAmount++
+    },
+    doLabor (state){
+        console.log(labor.name)
+        labor.purchased++
+        state.energyAmount -= labor.costEnergy
+        state.moneyAmount += labor.earningMoney
+    },
+    buyProducer (state, producerID){
+        state.producers = state.producers.map(producer => {
+            if (producer.id === producerID) {
+                console.log(producer.name)
+                producer.purchased++
+                state.energyAmount -= producer.costEnergy
+                state.moneyAmount -= producer.costMoney
+            }
+            return producer
+        })
+    }
+  }
+})
