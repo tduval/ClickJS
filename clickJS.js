7de1ca4c975b7de685a69ed32767d27e1e156115
Thomas Duval <Thomas.Duval@emeriocorp.com>
2018-05-28 15:34:02 +0800

    Refactor the countMoneyPerSeconds into core variable

--- clickJS.js
+++ clickJS.js
@@ -1,8 +1,7 @@
 const labor =   {
         id: 0,
         name: 'Manual Labor',
-        costEnergy: 10,
-        earningMoney: 10,
+        earningMoney: 1,
         purchased: 0
     }
 
@@ -11,33 +10,24 @@ const vehicles = [
         id: 0,
         name: 'Bicycle',
         costMoney: 50,
-        costEnergy: 8,
         moneyPerSeconds: 1,
-        energyPerSeconds: 1,
         earningMoney: 15,
-        earningEnergy:0,
         purchased: 0
     },
     {
         id: 1,
         name: 'Scooter',
         costMoney: 500,
-        costEnergy: 5,
         moneyPerSeconds: 3,
-        energyPerSeconds: 2,
         earningMoney: 25,
-        earningEnergy:0,
         purchased: 0
     },
     {
         id: 2,
         name: 'Car',
         costMoney: 1000,
-        costEnergy: 10,
         moneyPerSeconds: 10,
-        energyPerSeconds: 3,
         earningMoney: 100,
-        earningEnergy:0,
         purchased: 0
     },
 ]
@@ -46,33 +36,35 @@ const store = new Vuex.Store({
   state: {
     clickAmount: 0,
     moneyAmount: 1000,
-    energyAmount: 10,
+    moneyPerSecond: 0,
     vehicles: vehicles,
     labor: labor
   },
   mutations: {
-    incrementMoney (state, amount=1){
-        state.moneyAmount += amount
-        console.log("Increment Money/s : ", amount)
-    },
-    incrementEnergy (state, amount=1){
-        state.energyAmount += amount
-        state.clickAmount++
-        console.log("Increment Energy")
-    },
-    doLabor (state){
-        console.log("Do : ", labor.name)
-        labor.purchased++
-        state.energyAmount -= labor.costEnergy
-        state.moneyAmount += labor.earningMoney
-    },
-    buyVehicle (state, vehicleID){
-        var vehicle = state.vehicles.find(vehicle => vehicle.id === vehicleID)
-        if (vehicle.id === vehicleID) {
-            vehicle.purchased++
-            state.moneyAmount -= vehicle.costMoney
-            console.log("Buy vehicle : ", vehicle.name)
-         }
+        incrementMoney (state){
+            state.moneyAmount += state.moneyPerSecond
+            console.log("Increment Money/s : ", state.moneyPerSecond)
+        },
+        doLabor (state){
+            labor.purchased++
+            state.clickAmount++
+            state.moneyAmount += labor.earningMoney
+            console.log("Do : ", labor.name)
+        },
+        buyVehicle (state, vehicleID){
+            var vehicle = state.vehicles.find(vehicle => vehicle.id === vehicleID)
+            if (vehicle.id === vehicleID) {
+                vehicle.purchased++
+                state.moneyAmount -= vehicle.costMoney
+                console.log("Buy vehicle : ", vehicle.name)
+             }
+        },
+        countMoneyPerSecond (state){
+            state.moneyPerSecond = 0
+            for (let vehicle of state.vehicles) {
+                state.moneyPerSecond += vehicle.moneyPerSeconds * vehicle.purchased
+            }
+            return state.moneyPerSecond
+        }
     }
-  }
 })
